a068488503a1abec422d7d4e42c4c6c9fff92eba
Lingani Tshuma <lingani.tshuma@absa.co.za>
2018-02-03 05:23:23 +0200

    first commit

---
+++ cookbooks/linux-lab/README.md
@@ -0,0 +1,4 @@
+# linux-lab
+
+TODO: Enter the cookbook description here.
+
---
+++ roles/linux-lab.rb
@@ -0,0 +1,3 @@
+name "linux-lab"
+description "Pluralsight Geekhut lab role for Linux servers"
+run_list "recipe[linux-lab::apache]"
